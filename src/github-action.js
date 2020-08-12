12a7649b6525e0fd008aa13ff92c430e1662ab0e
Restyled.io <commits@restyled.io>
2020-08-12 23:07:40 +0000

    Restyled by prettier

--- src/github-action.js
+++ src/github-action.js
@@ -1,10 +1,10 @@
 const core = require('@actions/core');
-const {setup_cml} = require('./utils');
+const { setup_cml } = require('./utils');
 
 (async () => {
   try {
     const version = core.getInput('version');
-    await setup_cml({version});
+    await setup_cml({ version });
   } catch (error) {
     core.setFailed(error.message);
   }
--- src/utils.js
+++ src/utils.js
@@ -2,38 +2,38 @@ const util = require('util');
 
 const execp = util.promisify(require('child_process').exec);
 const exec = async (command, opts) => {
-  return new Promise(function(resolve, reject) {
-    const {debug} = opts || {};
+  return new Promise(function (resolve, reject) {
+    const { debug } = opts || {};
 
     execp(command, (error, stdout, stderr) => {
-      if (debug)
-        console.log(`\nCommand: ${command}\n\t${stdout}\n\t${stderr}`);
+      if (debug) console.log(`\nCommand: ${command}\n\t${stdout}\n\t${stderr}`);
 
-      if (error)
-        reject(error);
+      if (error) reject(error);
 
       resolve((stdout || stderr).slice(0, -1));
     });
   });
 };
 
-const setup_cml = async opts => {
-  const {version} = opts;
+const setup_cml = async (opts) => {
+  const { version } = opts;
   let sudo = '';
   try {
     sudo = await exec('which sudo');
-  } catch (err) {
-  }
+  } catch (err) {}
 
   console.log('Uninstalling previous CML');
   await exec(
-      `${sudo} npm uninstall -g canvas vega vega-cli vega-lite @dvcorg/cml`);
+    `${sudo} npm uninstall -g canvas vega vega-cli vega-lite @dvcorg/cml`
+  );
 
   console.log(`Installing CML version ${version}`);
   await exec('npm config set user 0');
   await exec(
-      `${sudo} npm install -g canvas vega vega-cli vega-lite @dvcorg/cml${
-          version !== 'latest' ? `@${version}` : ''}`);
+    `${sudo} npm install -g canvas vega vega-cli vega-lite @dvcorg/cml${
+      version !== 'latest' ? `@${version}` : ''
+    }`
+  );
 };
 
 exports.exec = exec;
