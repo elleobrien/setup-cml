c4a37a4fc268afa4afe5c3c67939ab5eed1850d6
Restyled.io <commits@restyled.io>
2020-08-12 23:07:34 +0000

    Restyled by clang-format

--- src/utils.js
+++ src/utils.js
@@ -3,12 +3,14 @@ const util = require('util');
 const execp = util.promisify(require('child_process').exec);
 const exec = async (command, opts) => {
   return new Promise(function(resolve, reject) {
-    const { debug } = opts || {};
+    const {debug} = opts || {};
 
     execp(command, (error, stdout, stderr) => {
-      if (debug) console.log(`\nCommand: ${command}\n\t${stdout}\n\t${stderr}`);
+      if (debug)
+        console.log(`\nCommand: ${command}\n\t${stdout}\n\t${stderr}`);
 
-      if (error) reject(error);
+      if (error)
+        reject(error);
 
       resolve((stdout || stderr).slice(0, -1));
     });
@@ -16,24 +18,22 @@ const exec = async (command, opts) => {
 };
 
 const setup_cml = async opts => {
-  const { version } = opts;
+  const {version} = opts;
   let sudo = '';
   try {
     sudo = await exec('which sudo');
-  } catch (err) {}
+  } catch (err) {
+  }
 
   console.log('Uninstalling previous CML');
   await exec(
-    `${sudo} npm uninstall -g canvas vega vega-cli vega-lite @dvcorg/cml`
-  );
+      `${sudo} npm uninstall -g canvas vega vega-cli vega-lite @dvcorg/cml`);
 
   console.log(`Installing CML version ${version}`);
   await exec('npm config set user 0');
   await exec(
-    `${sudo} npm install -g canvas vega vega-cli vega-lite @dvcorg/cml${
-      version !== 'latest' ? `@${version}` : ''
-    }`
-  );
+      `${sudo} npm install -g canvas vega vega-cli vega-lite @dvcorg/cml${
+          version !== 'latest' ? `@${version}` : ''}`);
 };
 
 exports.exec = exec;
